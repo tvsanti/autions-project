b5796f0e2bd90416b537c8ace107f7c134fb6562
santiago <santithevenetvalles@gmail.com>
2023-01-25 17:54:54 +0100

    Proceso de mejora de los filtros de busqueda

--- proyecto-tfg-completo/src/components/Buscar/FiltrosVue.vue
+++ proyecto-tfg-completo/src/components/Buscar/FiltrosVue.vue
@@ -1,10 +1,59 @@
 <template>
   <div class="filtrosVue">
     <ul class="ul1">
-      <button><i class="fa-solid fa-list-check"></i>Categorias</button>
-      <button><i class="fa-sharp fa-solid fa-location-dot"></i>Localización</button>
-      <button><i class="fa-solid fa-money-bill"></i>Precio</button>
-      <button><i class="fa-solid fa-star-of-life"></i>Estado del Productos</button>
+      <div class="filtroLi">
+        <div class="filtro2">
+          <button><i class="fa-solid fa-list-check"></i>Categorias</button>
+          <i
+            :class="{ flechaIconoAnimacion: animacionActiva }"
+            @click="rotarIconos()"
+            class="fa-solid fa-caret-left flechaIcono"
+          ></i>
+        </div>
+        <div :class="{mostarLi2: animacionActiva}" class="mostrarLi">
+          <select name="" id="">
+            <option value="">1</option>
+            <option value="">1</option>
+            <option value="">1</option>
+            <option value="">1</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="filtroLi">
+        <div class="filtro2">
+          <button>
+            <i class="fa-sharp fa-solid fa-location-dot"></i>Localización
+          </button>
+          <i
+            :class="{ flechaIconoAnimacion: animacionActiva }"
+            @click="rotarIconos()"
+            class="fa-solid fa-caret-left flechaIcono"
+          ></i>
+        </div>
+      </div>
+      <div class="filtroLi">
+        <div class="filtro2">
+          <button><i class="fa-solid fa-money-bill"></i>Precio</button>
+          <i
+            :class="{ flechaIconoAnimacion: animacionActiva }"
+            @click="rotarIconos()"
+            class="fa-solid fa-caret-left flechaIcono"
+          ></i>
+        </div>
+      </div>
+      <div class="filtroLi">
+        <div class="filtro2">
+          <button>
+            <i class="fa-solid fa-star-of-life"></i>Estado del Productos
+          </button>
+          <i
+            :class="{ flechaIconoAnimacion: animacionActiva }"
+            @click="rotarIconos()"
+            class="fa-solid fa-caret-left flechaIcono"
+          ></i>
+        </div>
+      </div>
     </ul>
     <ul class="ul2">
       <button>Ordenar Por</button>
@@ -13,13 +62,20 @@
 </template>
 
 <script>
-
 export default {
   name: "App",
-  components: {
+  components: {},
+  data() {
+    return {
+      animacionActiva: false,
+    };
+  },
+  methods: {
+    rotarIconos() {
+      this.animacionActiva = !this.animacionActiva;
+    },
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
