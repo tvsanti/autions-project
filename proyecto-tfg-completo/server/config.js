5ff3502558ba15843e86ba5dc4be657dfde3bd39
santiago <santithevenetvalles@gmail.com>
2023-05-14 12:24:03 +0200

    puertos railway

--- proyecto-tfg-completo/server/config.js
+++ proyecto-tfg-completo/server/config.js
@@ -5,11 +5,7 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
-console.log(PORT,
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_DATABASE);
+
 module.exports = {
   PORT,
   DB_HOST,
