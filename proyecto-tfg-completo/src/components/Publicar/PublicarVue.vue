82bb629f31eecd1c1b28ca593f1deba2bd79d129
santiago <santithevenetvalles@gmail.com>
2023-05-21 11:53:55 +0200

    Redireccion publicar

--- proyecto-tfg-completo/src/components/Publicar/PublicarVue.vue
+++ proyecto-tfg-completo/src/components/Publicar/PublicarVue.vue
@@ -82,7 +82,6 @@ export default {
         productos.append("files", file);
       });
 
-<<<<<<< HEAD
       await axios.post('http://167.99.240.123:81/api/localimages/productos', productos);
       
       let cookie = this.$cookies.get('loginCookie')
@@ -92,18 +91,6 @@ export default {
       window.location.href = 'http://167.99.240.123:80/'
 
 
-=======
-      await axios.post(
-        "http://localhost:3001/api/localimages/productos",
-        productos
-      );
-
-      let cookie = this.$cookies.get("loginCookie");
-      this.formulario.created_by = cookie.id_cliente;
-      let objeto = JSON.parse(JSON.stringify(this.formulario));
-      await axios.post("http://localhost:3001/api", objeto);
-      window.location.href = "http://localhost:8080/";
->>>>>>> main
     },
     onFileChange(event) {
       const files = this.$refs.files.files;
