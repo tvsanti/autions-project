ebb3c11c16eb20592c42a1f170920685da5fb27c
santiago <santithevenetvalles@gmail.com>
2023-05-27 12:00:46 +0200

    mover carpeta

--- proyecto-tfg-completo/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import MainVue from './MainVue.vue'
-import router from './router/index'
-import store from './store'
-import VueCookies from 'vue-cookies'
-
-createApp(MainVue).use(VueCookies,{expires: '3d'}).use(store).use(router).mount('#app')
